0e7610611bde31cfa6f9787d4e9e85e026766194
John Zhuge <jzhuge@cloudera.com>
2017-09-09 20:51:03 -0700

    Add complex expiration tests
    - testExpireMany
    - testExpireRemoved
    - testRemoveExpired
    - testExpireReadded

--- src/test/java/TestExpireMap.java
+++ src/test/java/TestExpireMap.java
@@ -6,11 +6,15 @@ import org.junit.Test;
 
 
 /**
- * Add a class comment here
+ * Test ExpireMap implementations.
  */
 public class TestExpireMap {
   static final String K1 = "key1";
   static final String V1 = "value1";
+  static final String K2 = "key2";
+  static final String V2 = "value2";
+  static final String K3 = "key3";
+  static final String V3 = "value3";
   
   private ExpireMap<String, String> emap;
 
@@ -22,17 +26,86 @@ public class TestExpireMap {
   public void testBasic() {
     Assert.assertThat(null, CoreMatchers.is(emap.get(K1)));
     emap.put(K1, V1, TimeUnit.SECONDS.toMillis(1));
-    Assert.assertThat(V1, CoreMatchers.is(emap.get(K1)));
+    Assert.assertThat("K1 added", V1, CoreMatchers.is(emap.get(K1)));
     emap.remove(K1);
-    Assert.assertThat(null, CoreMatchers.is(emap.get(K1)));
+    Assert.assertThat("K1 removed", null, CoreMatchers.is(emap.get(K1)));
   }
 
   @Test
-  public void testExpire() throws InterruptedException {
+  public void testExpireOne() throws InterruptedException {
+    // t+0: add K1 expiring at t+2
     emap.put(K1, V1, TimeUnit.SECONDS.toMillis(2));
+
     Thread.sleep(TimeUnit.SECONDS.toMillis(1));
-    Assert.assertThat(V1, CoreMatchers.is(emap.get(K1)));
+    // t+1
+    Assert.assertThat("K1 still there", V1, CoreMatchers.is(emap.get(K1)));
+
     Thread.sleep(TimeUnit.SECONDS.toMillis(2));
-    Assert.assertThat(null, CoreMatchers.is(emap.get(K1)));
+    // t+3
+    Assert.assertThat("K1 expired",  null, CoreMatchers.is(emap.get(K1)));
+  }
+
+  @Test
+  public void testExpireMany() throws InterruptedException {
+    // t+0: add K1 expiring at t+3
+    emap.put(K1, V1, TimeUnit.MILLISECONDS.toMillis(3000));
+
+    Thread.sleep(TimeUnit.MILLISECONDS.toMillis(500));
+    // t+0.5: add K2 expiring at t+1, add K3 expiring at t+2
+    emap.put(K2, V2, TimeUnit.MILLISECONDS.toMillis(500));
+    emap.put(K3, V3, TimeUnit.MILLISECONDS.toMillis(1500));
+
+    Thread.sleep(TimeUnit.MILLISECONDS.toMillis(1000));
+    // t+1.5
+    Assert.assertThat("K1 still there", V1, CoreMatchers.is(emap.get(K1)));
+    Assert.assertThat("K2 expired", null, CoreMatchers.is(emap.get(K2)));
+    Assert.assertThat("K3 still there", V3, CoreMatchers.is(emap.get(K3)));
+
+    Thread.sleep(TimeUnit.MILLISECONDS.toMillis(1000));
+    // t+2.5
+    Assert.assertThat("K1 still there", V1, CoreMatchers.is(emap.get(K1)));
+    Assert.assertThat("K3 expired", null, CoreMatchers.is(emap.get(K3)));
+
+    Thread.sleep(TimeUnit.MILLISECONDS.toMillis(1000));
+    // t+3.5
+    Assert.assertThat("K1 expired", null, CoreMatchers.is(emap.get(K1)));
+  }
+
+  @Test
+  public void testExpireRemoved() throws InterruptedException {
+    // t+0: add K1 expiring at t+2
+    emap.put(K1, V1, TimeUnit.SECONDS.toMillis(2));
+
+    Thread.sleep(TimeUnit.SECONDS.toMillis(1));
+    // t+1
+    emap.remove(K1);
+
+    Thread.sleep(TimeUnit.SECONDS.toMillis(2));
+    // t+3: no news is good news
+  }
+
+  @Test
+  public void testRemoveExpired() throws InterruptedException {
+    // t+0: add K1 expiring at t+1
+    emap.put(K1, V1, TimeUnit.SECONDS.toMillis(1));
+
+    Thread.sleep(TimeUnit.SECONDS.toMillis(2));
+    // t+2: remove the expired K1
+    emap.remove(K1);
+  }
+
+  @Test
+  public void testExpireReadded() throws InterruptedException {
+    // t+0: add K1 expiring at t+2
+    emap.put(K1, V1, TimeUnit.SECONDS.toMillis(2));
+
+    Thread.sleep(TimeUnit.SECONDS.toMillis(1));
+    // t+1
+    emap.remove(K1);
+    emap.put(K1, V1, TimeUnit.SECONDS.toMillis(100));
+
+    Thread.sleep(TimeUnit.SECONDS.toMillis(2));
+    // t+3: K1 should be still there
+    Assert.assertThat("K1 still there", V1, CoreMatchers.is(emap.get(K1)));
   }
 }
